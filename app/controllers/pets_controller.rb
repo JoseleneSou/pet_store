12c1888e12f30f5ee8cf8652992c2c45558657a5
Joselene <joselene.fiscal@gmail.com>
2022-05-07 08:42:58 -0300

    Add pets_controller and views

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,30 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def banana
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to root_path, notice: "Pet cadastrado com sucesso"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :age)
+  end
+end
