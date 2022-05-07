4c72b989677e4f4ae2d74910ac2102732433a8dc
Joselene <joselene.fiscal@gmail.com>
2022-05-07 14:46:00 -0300

    add links to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/pets', to: 'pets#index'
+  get '/pets', to: 'pets#index' 
   get '/pets/new', to: 'pets#new'
   post '/pets', to: 'pets#create'
-  get '/pet/:id', to: 'pets#show'
+  get '/pet/:id', to: 'pets#show', as: :pet
   
   # TODO: remove example route
   get '/banana', to: 'pets#banana'
