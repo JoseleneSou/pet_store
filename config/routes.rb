80e4e85f09e7795809ab8a21644341efacd38fe2
Joselene <joselene.fiscal@gmail.com>
2022-05-07 08:33:31 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/show', to: 'pets#show'
+  get '/pets', to: 'pets#index'
+  get '/pets/new', to: 'pets#new'
+  post '/pets', to: 'pets#create'
+  
+  # TODO: remove example route
+  get '/banana', to: 'pets#banana'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
