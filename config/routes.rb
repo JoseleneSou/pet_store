5a0e935e8e6826eba29bc972bdead7eb42bcab8b
Joselene <joselene.fiscal@gmail.com>
2022-05-14 08:00:07 -0300

    Add models and views for contacts controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/pets/new', to: 'pets#new'
   post '/pets', to: 'pets#create'
   get '/pet/:id', to: 'pets#show', as: :pet
+  get '/contacts', to: 'contacts#new'
+  post '/contacts', to: 'contacts#create'
   
   # TODO: remove example route
   get '/banana', to: 'pets#banana'
